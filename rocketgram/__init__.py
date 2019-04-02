7a097aef8a109a2c869b127966686d0e48772ae9
vd <vd@>
2019-04-02 17:50:53 +0300

    Fixed imports.

--- rocketgram/__init__.py
+++ rocketgram/__init__.py
@@ -2,18 +2,15 @@
 # This file is part of RocketGram, the modern Telegram bot framework.
 # RocketGram is released under the MIT License (see LICENSE).
 
-from .types import *
-from .errors import *
-from .requests import *
-from .update import *
-from .inlineresults import *
-from .keyboards import *
+from . import tools
 from .bot import Bot
 from .context import Context
-from .middlewares import *
-from .routers import *
+from .errors import *
 from .executors import *
-from . import tools
-
-
-from .version import version
+from .inlineresults import *
+from .keyboards import *
+from .middlewares import *
+from .requests import *
+from .routers import *
+from .types import *
+from .update import *
