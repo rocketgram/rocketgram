0674b92fc5bfaf858aa72a57e7a11237a2033c6d
vd <vd@>
2019-04-02 17:56:38 +0300

    Fixed import again.

--- rocketgram/__init__.py
+++ rocketgram/__init__.py
@@ -2,15 +2,22 @@
 # This file is part of RocketGram, the modern Telegram bot framework.
 # RocketGram is released under the MIT License (see LICENSE).
 
-from . import tools
-from .bot import Bot
-from .context import Context
-from .errors import *
-from .executors import *
-from .inlineresults import *
-from .keyboards import *
-from .middlewares import *
-from .requests import *
-from .routers import *
-from .types import *
-from .update import *
+try:
+    from . import tools
+    from .bot import Bot
+    from .context import Context
+    from .errors import *
+    from .executors import *
+    from .inlineresults import *
+    from .keyboards import *
+    from .middlewares import *
+    from .requests import *
+    from .routers import *
+    from .types import *
+    from .update import *
+except ModuleNotFoundError as e:
+    print('Fail to import requirements: ', str(e))
+
+
+def version():
+    return "2019.03"
--- rocketgram/version.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2015-2019 by Vd.
-# This file is part of RocketGram, the modern Telegram bot framework.
-# RocketGram is released under the MIT License (see LICENSE).
-
-
-def version():
-    return "2019.03"
