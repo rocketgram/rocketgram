a11015287d709825ee41c9ce379345da4296f929
vd <vd@>
2019-04-18 18:09:18 +0300

    Makes LimiterMiddleware compatible to multibot environment.

--- rocketgram/middlewares/limiter.py
+++ rocketgram/middlewares/limiter.py
@@ -7,6 +7,7 @@ import typing
 from time import monotonic
 
 if typing.TYPE_CHECKING:
+    from ..bot import Bot
     from ..context import Context
 
 from .middleware import EmptyMiddleware
@@ -23,18 +24,24 @@ class LimiterMiddleware(EmptyMiddleware):
     def __init__(self, quantity: int, period: float):
         self.__quantity = quantity
         self.__period = period
-        self.__values = list()
+        self.__values = dict()
+
+    def shutdown(self, bot: 'Bot'):
+        bot_id = id(bot)
+        if bot_id in self.__values:
+            del self.__values[bot_id]
 
     def process(self, context: 'Context') -> 'Context':
+        bot_id = id(context.bot)
         current = monotonic()
         d = current - self.__period
 
-        self.__values = [v for v in self.__values if v > d]
+        self.__values[bot_id] = [v for v in self.__values.get(bot_id, list()) if v > d]
 
-        if len(self.__values) >= self.__quantity:
+        if len(self.__values[bot_id]) >= self.__quantity:
             raise RocketgramStopRequest(f'Update `{context.update.update_id}` was dropped due to '
-                                        f'rate exceed `{self.__quantity}` msg per `{self.__period} secs.`')
+                                        f'rate exceed `{self.__quantity}` msg per `{self.__period}` secs.')
 
-        self.__values.append(current)
+        self.__values[bot_id].append(current)
 
         return context
