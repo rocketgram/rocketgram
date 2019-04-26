8d55e59399091ad1d08b47c36b5f348238dbf7d9
vd <vd@>
2019-04-26 19:06:27 +0300

    Reformat setup.py.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # This file is part of Rocketgram, the modern Telegram bot framework.
 # Rocketgram is released under the MIT License (see LICENSE).
 
+
 from os.path import join, dirname
 
 import setuptools
