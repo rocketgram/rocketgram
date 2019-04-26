fbddf006b92906596ef01fe2a0063ad15d65b0a5
vd <vd@>
2019-04-26 19:38:33 +0300

    Fixed format.

--- rocketgram/__init__.py
+++ rocketgram/__init__.py
@@ -2,6 +2,7 @@
 # This file is part of Rocketgram, the modern Telegram bot framework.
 # Rocketgram is released under the MIT License (see LICENSE).
 
+
 from contextlib import suppress
 
 with suppress(ModuleNotFoundError):
