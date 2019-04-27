b10de38638635b32796b3195629a78dd98ccb878
vd <vd@>
2019-04-27 16:35:13 +0300

    More tests.

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -2,6 +2,7 @@
 # This file is part of Rocketgram, the modern Telegram bot framework.
 # Rocketgram is released under the MIT License (see LICENSE).
 
+
 from io import BytesIO
 
 from rocketgram import requests
@@ -66,4 +67,34 @@ def test_GetMe():
     assert req.render() == {}
     assert req.method == 'getMe'
     assert req.render(with_method=True) == {'method': 'getMe'}
+    assert req.files() == []
+
+
+def test_SendMessage():
+    req = requests.SendMessage(1000, "Hello, World!")
+    assert req.render() == {'chat_id': 1000, 'text': 'Hello, World!'}
+    assert req.method == 'sendMessage'
+    assert req.render(with_method=True) == {'method': 'sendMessage', 'chat_id': 1000, 'text': 'Hello, World!'}
+    assert req.files() == []
+
+    kb = types.InlineKeyboardMarkup([[types.InlineKeyboardButton('Button', callback_data='data')]])
+    req = requests.SendMessage(1000, "Hello, World!", parse_mode=types.ParseModeType.html,
+                               disable_web_page_preview=True, disable_notification=True, reply_to_message_id=100,
+                               reply_markup=kb)
+
+    assert req.render() == {'chat_id': 1000, 'text': 'Hello, World!', 'disable_notification': True,
+                            'disable_web_page_preview': True, 'parse_mode': 'html', 'reply_to_message_id': 100,
+                            'reply_markup': {'inline_keyboard': [[{'callback_data': 'data', 'text': 'Button'}]]}}
+
+
+def test_ForwardMessage():
+    req = requests.ForwardMessage(1000, 1234, 100)
+    assert req.render() == {'chat_id': 1000, 'from_chat_id': 1234, 'message_id': 100}
+    assert req.method == 'forwardMessage'
+    assert req.render(with_method=True) == {'method': 'forwardMessage', 'chat_id': 1000, 'from_chat_id': 1234,
+                                            'message_id': 100}
+    assert req.files() == []
+
+    req = requests.ForwardMessage(1000, 1234, 100, disable_notification=True)
+    assert req.render() == {'chat_id': 1000, 'from_chat_id': 1234, 'message_id': 100, 'disable_notification': True}
     assert req.files() == []
