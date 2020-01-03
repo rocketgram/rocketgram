d408614d37774563e6dd198bed0edac68253a42e
vd <vd@>
2020-01-04 00:16:32 +0200

    Changed date in license.

--- rocketgram/__init__.py
+++ rocketgram/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2019 by Vd.
+# Copyright (C) 2015-2020 by Vd.
 # This file is part of Rocketgram, the modern Telegram bot framework.
 # Rocketgram is released under the MIT License (see LICENSE).
 
