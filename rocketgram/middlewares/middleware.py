00ec80556146da4331d38c199ec0d73dc3ec407d
vd <vd@>
2020-01-04 04:00:44 +0200

    More typing.

--- rocketgram/middlewares/middleware.py
+++ rocketgram/middlewares/middleware.py
@@ -3,60 +3,60 @@
 # Rocketgram is released under the MIT License (see LICENSE).
 
 
-import typing
+from typing import TYPE_CHECKING, Awaitable, Union, Optional
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from ..update import Response
     from ..requests import Request
 
 
 class Middleware:
-    def init(self):
+    def init(self) -> Optional[Awaitable]:
         raise NotImplementedError
 
-    def shutdown(self):
+    def shutdown(self) -> Optional[Awaitable]:
         raise NotImplementedError
 
-    def before_process(self):
+    def before_process(self) -> Optional[Awaitable]:
         raise NotImplementedError
 
-    def after_process(self):
+    def after_process(self) -> Optional[Awaitable]:
         raise NotImplementedError
 
-    def process_error(self, error: Exception):
+    def process_error(self, error: Exception) -> Optional[Awaitable]:
         raise NotImplementedError
 
-    def before_request(self, request: 'Request') -> 'Request':
+    def before_request(self, request: 'Request') -> Union['Response', Awaitable['Request']]:
         raise NotImplementedError
 
-    def after_request(self, request: 'Request', response: 'Response') -> 'Response':
+    def after_request(self, request: 'Request', response: 'Response') -> Union['Response', Awaitable['Response']]:
         raise NotImplementedError
 
-    def request_error(self, request: 'Request', error: Exception):
+    def request_error(self, request: 'Request', error: Exception) -> Optional[Awaitable]:
         raise NotImplementedError
 
 
 class EmptyMiddleware(Middleware):
-    def init(self):
+    def init(self) -> Optional[Awaitable]:
         pass
 
-    def shutdown(self):
+    def shutdown(self) -> Optional[Awaitable]:
         pass
 
-    def before_process(self):
+    def before_process(self) -> Optional[Awaitable]:
         pass
 
-    def after_process(self):
+    def after_process(self) -> Optional[Awaitable]:
         pass
 
-    def process_error(self, error: Exception):
+    def process_error(self, error: Exception) -> Optional[Awaitable]:
         pass
 
-    def before_request(self, request: 'Request') -> 'Request':
+    def before_request(self, request: 'Request') -> Union['Response', Awaitable['Request']]:
         return request
 
-    def after_request(self, request: 'Request', response: 'Response') -> 'Response':
+    def after_request(self, request: 'Request', response: 'Response') -> Union['Response', Awaitable['Response']]:
         return response
 
-    def request_error(self, request: 'Request', error: Exception):
+    def request_error(self, request: 'Request', error: Exception) -> Optional[Awaitable]:
         pass
