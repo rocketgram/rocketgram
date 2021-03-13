fe9e4cec16d8e104389b89cb1712ca0c7ae15c4c
vd <vd@>
2021-03-13 20:37:41 +0200

    Fixed formatting.

--- rocketgram/api/__init__.py
+++ rocketgram/api/__init__.py
@@ -2,6 +2,7 @@
 # This file is part of Rocketgram, the modern Telegram bot framework.
 # Rocketgram is released under the MIT License (see LICENSE).
 
+
 from .add_sticker_to_set import AddStickerToSet
 from .animation import Animation
 from .answer_callback_query import AnswerCallbackQuery
