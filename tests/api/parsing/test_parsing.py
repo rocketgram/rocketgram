340cf521339649c83307a9a4716f572b0c6f3b97
vd <33198864+vd2org@users.noreply.github.com>
2023-07-21 23:59:24 +0400

    Implemented the new tests.

---
+++ tests/api/parsing/test_parsing.py
@@ -0,0 +1,93 @@
+import os
+from dataclasses import asdict
+from datetime import datetime
+from enum import Enum
+from logging import getLogger
+from typing import Sequence
+
+import pytest
+import yaml
+
+from rocketgram import Update, UpdateType, MessageType, ChatType
+
+logger = getLogger(__name__)
+
+
+class ParsingTest:
+    @staticmethod
+    def collect(path: str) -> Sequence[str]:
+        for root, _, files in os.walk(path):
+            for file in files:
+                if file.endswith('.yaml'):
+                    yield os.path.relpath(os.path.join(root, file), start=path)
+
+    def __init__(self, file_name: str):
+        self._name = file_name
+
+        with open(os.path.join(os.path.dirname(__file__), file_name), 'r') as f:
+            data = yaml.safe_load(f.read())
+
+        self._description = data['description']
+        self._input = data['input']
+        exp = data['expected']
+
+        self._e_update_type = UpdateType(exp.get('update_type')) if exp.get('update_type') else None
+        self._e_message_type = MessageType(exp.get('message_type')) if exp.get('message_type') else None
+        self._e_chat_type = ChatType(exp.get('chat_type')) if exp.get('chat_type') else None
+
+        self._e_output = exp['output']
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def input(self) -> dict:
+        return self._input
+
+    def compare(self, update: Update):
+        if self._e_update_type:
+            assert update.type == self._e_update_type, f"Update type mismatch"
+        if self._e_message_type:
+            assert update.message.type == self._e_message_type, f"Message type mismatch"
+        if self._e_chat_type:
+            assert update.message.chat.type == self._e_chat_type, f"Chat type mismatch"
+
+        update_dict = asdict(update)
+        assert update_dict['raw'] == self._input, f"`raw` field is not equal to `input`"
+        del update_dict['raw']
+
+        self._check_extra_fields(update_dict, self._e_output)
+
+    @classmethod
+    def _check_extra_fields(cls, element: dict, expected: dict, path: str = ''):
+        # Check extra fields
+        for k in set(element.keys()) - set(expected.keys()):
+            assert element[k] is None, f"Field `{path}{k}` is none"
+
+        # Check expected fields
+        for k, ev in expected.items():
+            v = element.get(k)
+
+            if isinstance(v, dict):
+                cls._check_extra_fields(element[k], expected[k], f"{path}{k}.")
+                continue
+
+            if isinstance(v, Enum):
+                v = v.value
+            elif isinstance(v, datetime):
+                v = int(v.timestamp())
+                continue  # TODO: remove after fix #12
+
+            assert ev == v, f"Field `{path}{k}`: expected `{ev}`, got `{v}`"
+
+
+@pytest.fixture(scope="module", params=ParsingTest.collect(os.path.dirname(__file__)))
+def parsing_test(request):
+    return ParsingTest(request.param)
+
+
+@pytest.mark.api
+def test_parsing(parsing_test: ParsingTest):
+    logger.debug("Parsing test: %s", parsing_test.name)
+    parsing_test.compare(Update.parse(parsing_test.input))
---
+++ tests/dispatcher/commonfilters/test_command.py
@@ -0,0 +1,76 @@
+import asyncio
+from logging import getLogger
+from typing import Tuple
+
+import pytest
+
+from rocketgram import Bot, Update, Dispatcher, Connector
+from rocketgram import context as ctx
+from rocketgram.routers.dispatcher import commonfilters
+
+logger = getLogger(__name__)
+
+
+def make_bot(bot_name: str = "TestBot") -> Tuple[Bot, Dispatcher]:
+    connector = Connector()
+    dispatcher = Dispatcher()
+    bot = Bot("1234567890:AAgLHoRpZxIcND0EqNL15HurLzwwSKomngX", router=dispatcher, connector=connector)
+    bot.name = bot_name
+
+    return bot, dispatcher
+
+
+def make_update(text: str):
+    return Update.parse(
+        {
+            "update_id": 123456789,
+            "message": {
+                "message_id": 1234567,
+                "from": {
+                    "id": 123456789,
+                    "is_bot": False,
+                    "first_name": "User",
+                    "username": "username",
+                    "language_code": "en",
+                    "is_premium": True
+                },
+                "chat": {
+                    "id": 123456789,
+                    "first_name": "User",
+                    "username": "username",
+                    "type": "private"
+                },
+                "date": 1691234567,
+                "text": text
+            }
+        }
+    )
+
+
+@pytest.mark.dispatcher
+def test_command():
+    bot, dispatcher = make_bot()
+
+    results = []
+
+    @dispatcher.handler
+    @commonfilters.command("/start")
+    async def handler():
+        results.append(ctx.update)
+
+    update_expected_1 = make_update("/start")
+    update_expected_2 = make_update("/start@TestBot")
+    update_unexpected_1 = make_update("/start@UnknownBot")
+    update_unexpected_2 = make_update("/stop")
+
+    asyncio.run(bot.process(None, update_expected_1))
+    asyncio.run(bot.process(None, update_expected_2))
+    asyncio.run(bot.process(None, update_unexpected_1))
+    asyncio.run(bot.process(None, update_unexpected_2))
+
+    assert update_expected_1 in results
+    assert update_expected_2 in results
+    assert update_unexpected_1 not in results
+    assert update_unexpected_2 not in results
+
+    assert len(results) == 2
