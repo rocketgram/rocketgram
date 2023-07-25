928890a32852929afec490d2d77b838dd806bfe9
vd <33198864+vd2org@users.noreply.github.com>
2023-07-25 22:35:48 +0400

    Updated copyright.

--- rocketgram/tools/escape.py
+++ rocketgram/tools/escape.py
@@ -1,4 +1,4 @@
-﻿# Copyright (C) 2015-2022 by Vd.
+﻿# Copyright (C) 2015-2023 by Vd.
 # This file is part of Rocketgram, the modern Telegram bot framework.
 # Rocketgram is released under the MIT License (see LICENSE).
 
