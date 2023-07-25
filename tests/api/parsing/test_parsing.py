1de65207fa6733c7c0d02fc529b2061a324a46cf
vd <33198864+vd2org@users.noreply.github.com>
2023-07-25 22:45:28 +0400

    Added copyrights.

--- tests/api/parsing/test_parsing.py
+++ tests/api/parsing/test_parsing.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2015-2023 by Vd.
+# This file is part of Rocketgram, the modern Telegram bot framework.
+# Rocketgram is released under the MIT License (see LICENSE).
+
+
 import os
 from dataclasses import asdict
 from datetime import datetime
